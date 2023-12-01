8c0656336bbcafd34798b44a5c3e79e826713ebb
Jay2cooo <145384928+Jay2cooo@users.noreply.github.com>
2023-12-01 07:01:54 -0800

    Added to new script
    added to new script

--- CroffieRubysAdventureP3/Assets/Scripts/RubyController.cs
+++ CroffieRubysAdventureP3/Assets/Scripts/RubyController.cs
@@ -18,7 +18,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -38,7 +38,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    void ChangeHealth (int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
